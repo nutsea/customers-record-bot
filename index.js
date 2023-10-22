dc18b15e6edadf7ce9a9329791e725d9a9fde3e8
Anastasia Batyrova <batyrova.n@icloud.com>
2023-10-22 04:07:25 +0300

    ssl links

--- index.js
+++ index.js
@@ -10,27 +10,43 @@ const botController = require('./controllers/botController.js')
 const { Telegraf, Markup, session } = require('telegraf');
 const bookingContoller = require('./controllers/bookingContoller.js')
 const clientController = require('./controllers/clientController.js')
+const path = require('path')
+const https = require('https')
+const fs = require('fs')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5100
+
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/bot.taxiallrussia.ru/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/bot.taxiallrussia.ru/cert.pem')
+}
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'works' })
-})
+const server = https.createServer(options, app)
 
 const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        server.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
-        console.error(e)
+        console.log(e)
     }
 }
+
+// const start = async () => {
+//     try {
+//         await sequelize.authenticate()
+//         await sequelize.sync()
+//         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+//     } catch (e) {
+//         console.error(e)
+//     }
+// }
 
 start()
 
